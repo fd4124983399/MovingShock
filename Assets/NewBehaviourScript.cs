4c4621d441382c5f0df467b8d4f7a868265010a5
NaI <fd4124983399@gmail.com>
2020-09-09 01:23:59 +0800

    First commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool UseMovePosition, UseFixedUpdate;
+    Rigidbody2D rigid;
+    Vector3 originPos;
+
+    void Awake(){
+        rigid = GetComponent<Rigidbody2D>();
+        originPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UseFixedUpdate)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            transform.position = originPos;
+        else
+            transform.position += Vector3.right * Time.deltaTime;
+    }
+
+    void FixedUpdate(){
+        if (!UseFixedUpdate)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rigid.position = originPos;
+        else{
+            if (UseMovePosition)
+                rigid.MovePosition(rigid.position + Vector2.right * 0.01f);
+            else
+                transform.position += Vector3.right * 0.01f;   
+        } 
+    }
+}
